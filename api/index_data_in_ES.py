11becd79c5de815c013956d6ec2c186090553195
Paul Girard <paul@ouestware.com>
2021-01-22 11:57:48 +0100

    new data version updates

--- api/index_data_in_ES.py
+++ api/index_data_in_ES.py
@@ -1,15 +1,16 @@
 #!python
 from elasticsearch import Elasticsearch, helpers
 import csv
-import ast 
+import ast
 import json
 from itertools import groupby
 import os
 
 from config import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT
-DELETE_INDEX=True
+DELETE_INDEX = True
 
-DOC_FIELDS_INTO_SEGMENTS = ['protocol_type_i_o_me', 'job_i_me', 'age_i_me', 'residence_i_me', 'housemates_i_me', 'platform_o', ]
+DOC_FIELDS_INTO_SEGMENTS = ['protocol_type_i_o_me', 'job_i_me',
+                            'age_i_me', 'residence_i_me', 'housemates_i_me', 'platform_o', ]
 
 
 if __name__ == '__main__':
@@ -19,151 +20,159 @@ if __name__ == '__main__':
         text_segment_mapping = json.load(mf)
 
     print("Starting data indexation")
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
     for index in ["text_segments", "documents"]:
-        if es.indices.exists(index=index ) and DELETE_INDEX:
-            print('index %s deleted'%index)
+        if es.indices.exists(index=index) and DELETE_INDEX:
+            print('index %s deleted' % index)
             es.indices.delete(index=index)
         if not es.indices.exists(index=index):
             es.indices.create(index=index)
             es.indices.close(index=index)
-        
+
     # in cas we need specific keyword, add this filter
     # "danish_keywords": {
     #             "type":       "keyword_marker",
-    #             "keywords":   ["eksempel"] 
+    #             "keywords":   ["eksempel"]
     #             },
     es.indices.put_settings(index='text_segments', body={
         "analysis": {
             "filter": {
                 "danish_stop": {
-                "type":       "stop",
-                "stopwords":  "_danish_" 
+                    "type":       "stop",
+                    "stopwords":  "_danish_"
                 },
                 "danish_stemmer": {
-                "type":       "stemmer",
-                "language":   "danish"
+                    "type":       "stemmer",
+                    "language":   "danish"
                 }
             },
             "analyzer": {
                 "rebuilt_danish": {
-                "tokenizer":  "standard",
-                "filter": [
-                    "lowercase",
-                    "danish_stop",
-                    #"danish_keywords",
-                    "danish_stemmer"
-                ]
+                    "tokenizer":  "standard",
+                    "filter": [
+                        "lowercase",
+                        "danish_stop",
+                        # "danish_keywords",
+                        "danish_stemmer"
+                    ]
                 }
             }
         }
     })
 
     if text_segment_mapping:
-        es.indices.put_mapping(index='text_segments', body = text_segment_mapping )
+        es.indices.put_mapping(index='text_segments',
+                               body=text_segment_mapping)
     for index in ["text_segments", "documents"]:
         es.indices.open(index=index)
 
     with open('./data/csv/text_segment.csv', 'r', encoding='utf8') as segments_f, open('./data/csv/text_segment_metadata.csv', 'r', encoding='utf8') as segments_meta_f, open('./data/csv/document_metadata.csv', 'r', encoding='utf8') as docs_meta_f:
         # hashmap of segments
-        segments_content = {s["text_segment_id"]:s for s in csv.DictReader(segments_f)}
+        segments_content = {s["text_segment_id"]
+            : s for s in csv.DictReader(segments_f)}
         # hashmap of docs
-        docs = {s["document_id"]:s for s in csv.DictReader(docs_meta_f)}
-        for _,doc in docs.items():
-            del(doc['']) #remove line number
-        
+        docs = {s["document_id"]: s for s in csv.DictReader(docs_meta_f)}
+
         segment_meta_CSV = csv.DictReader(segments_meta_f)
         all_segments = list(segment_meta_CSV)
-        all_segments.sort(key=  lambda s : s['document_id'])
-        
-        for doc_id, segments in groupby(all_segments, key= lambda s : s['document_id']):
+        all_segments.sort(key=lambda s: s['document_id'])
+
+        for doc_id, segments in groupby(all_segments, key=lambda s: s['document_id']):
             # reset order count
             order = 0
             segments = list(segments)
             nb_segments = len(segments)
             last_segment_id = ""
             segments_in_order = []
-            while len(segments_in_order)<nb_segments:
-                next_segment = [s for s in segments if s["text_segment_id_preceding"] == last_segment_id]
+            while len(segments_in_order) < nb_segments:
+                next_segment = [
+                    s for s in segments if s["text_segment_id_preceding"] == last_segment_id]
                 if len(next_segment) == 1:
                     next_segment = next_segment[0]
                     next_segment['order'] = order
                     # order variable will be easier
                     order += 1
                     # segment metadata management
-                    del(next_segment[''])
+
                     # lists
                     machine_tags = []
                     if next_segment['tfidf_tag'] == '':
-                        next_segment['tfidf_tag'] = [] 
+                        next_segment['tfidf_tag'] = []
                     else:
-                        next_segment['tfidf_tag'] = ast.literal_eval(next_segment['tfidf_tag'])
+                        next_segment['tfidf_tag'] = ast.literal_eval(
+                            next_segment['tfidf_tag'])
                         machine_tags += next_segment['tfidf_tag']
                     if next_segment['ner_tag'] == '':
                         next_segment['ner_tag'] = []
                     else:
-                        next_segment['ner_tag'] = ast.literal_eval(next_segment['ner_tag'])
+                        next_segment['ner_tag'] = ast.literal_eval(
+                            next_segment['ner_tag'])
                         machine_tags += next_segment['ner_tag']
                     next_segment['machine_tags'] = machine_tags
                     # force image into list
-                    
-                    if next_segment['image_o_me'] != "" and next_segment['image_o_me'][0] == '[':
-                        next_segment['image_o_me'] = ast.literal_eval(next_segment['image_o_me'])
+
+                    if next_segment['images_me_o'] != "" and next_segment['images_me_o'][0] == '[':
+                        next_segment['images_me_o'] = ast.literal_eval(
+                            next_segment['images_me_o'])
                     else:
-                        next_segment['image_o_me'] = [next_segment['image_o_me']] if next_segment['image_o_me']!="" else []
-                    if  next_segment['text_segment_similarity_id'] == '':
+                        next_segment['images_me_o'] = [
+                            next_segment['images_me_o']] if next_segment['images_me_o'] != "" else []
+                    if next_segment['text_segment_similarity_id'] == '':
                         next_segment['text_segment_similarity_id'] = []
                     else:
-                        next_segment['text_segment_similarity_id'] = ast.literal_eval(next_segment['text_segment_similarity_id'])
-                    
+                        next_segment['text_segment_similarity_id'] = ast.literal_eval(
+                            next_segment['text_segment_similarity_id'])
+
                     # nan in date
-                    next_segment['date_i_o_me'] = next_segment['date_i_o_me'].replace(" nan", "")
+                    next_segment['date_i_o_me'] = next_segment['date_i_o_me'].replace(
+                        " nan", "")
                     # content
                     if next_segment['text_segment_id'] in segments_content:
                         sc = segments_content[next_segment['text_segment_id']]
                         next_segment["text_question"] = sc['text_question']
                         next_segment["text_answer"] = sc['text_answer']
                     else:
-                        raise Exception("segment ID %s not found in segment content CSV"%s['text_segment_id'])
+                        raise Exception(
+                            "segment ID %s not found in segment content CSV" % s['text_segment_id'])
                     # doc meta injected to ease search
                     if next_segment['document_id'] in docs:
                         doc = docs[next_segment['document_id']]
                         for f in DOC_FIELDS_INTO_SEGMENTS:
                             next_segment[f] = doc[f]
                     else:
-                        raise Exception("segment ID %s has an unknow doc id %s"%(next_segment['text_segment_id'], next_segment['document_id']))
+                        raise Exception("segment ID %s has an unknow doc id %s" % (
+                            next_segment['text_segment_id'], next_segment['document_id']))
 
                     segments_in_order.append(next_segment)
                     last_segment_id = next_segment["text_segment_id"]
                 else:
-                    raise Exception('segment sequence broken %s'%segments)
+                    raise Exception('segment sequence broken %s' % segments)
 
         # index batch to ES
         index_result, _ = helpers.bulk(es, ({
             "_op_type": "update",
             "doc_as_upsert": True,
             "_id": s['text_segment_id'],
-            'doc':s}
-                for s in all_segments),
+            'doc': s}
+            for s in all_segments),
             index='text_segments')
         if index_result > 0:
-            print("%s text_segments inserted"%(index_result))
-        
-
+            print("%s text_segments inserted" % (index_result))
 
         # documents
+
         def cast_doc(d):
-            d['document_tags_i'] = ast.literal_eval(d['document_tags_i'] if d['document_tags_i'] !='' else "[]")
+            d['document_tags_i'] = d['document_tags_i'].split(', ')
             return d
         index_result, _ = helpers.bulk(es, ({
             "_op_type": "update",
             "doc_as_upsert": True,
             "_id": doc_id,
             'doc': cast_doc(d)}
-                for doc_id,d in docs.items()),
+            for doc_id, d in docs.items()),
             index='documents')
         if index_result > 0:
-            print("%s documents inserted"%(index_result))
+            print("%s documents inserted" % (index_result))
 
         # user tags
         if os.path.exists("./data/csv/user_tags.csv"):
@@ -172,12 +181,13 @@ if __name__ == '__main__':
                 tags_updates = {}
                 for seg_tags in tags_csv:
                     # if the same segment is listed twice we take the last one only
-                    tags_updates[seg_tags['text_segment_id']] = {"user_tags": ast.literal_eval(seg_tags["user_tags"])}
+                    tags_updates[seg_tags['text_segment_id']] = {
+                        "user_tags": ast.literal_eval(seg_tags["user_tags"])}
                 index_result, _ = helpers.bulk(es, ({
                     "_op_type": "update",
                     "_id": seg_id,
                     'doc': d}
-                        for seg_id,d in tags_updates.items()),
+                    for seg_id, d in tags_updates.items()),
                     index='text_segments')
                 if index_result > 0:
-                    print("tags inserted in %s segments"%(index_result))
+                    print("tags inserted in %s segments" % (index_result))
--- api/models.py
+++ api/models.py
@@ -2,39 +2,41 @@
 
 def _translate(o, translation):
     new_o = {}
-    for f,of in translation.items():
+    for f, of in translation.items():
         if isinstance(of, list):
-            if len([off for off in of if off in o]) > 0: 
+            if len([off for off in of if off in o]) > 0:
                 new_o[f] = [ssf for sf in of for ssf in o[sf]]
         elif of in o:
             new_o[f] = o[of]
     return new_o
 
+
 def segment(es_segment):
     meta_alignement = {
         "id": 'text_segment_id',
-        "question": 'text_question', 
+        "question": 'text_question',
         "answer": 'text_answer',
-        "images": 'image_o_me',
+        "images": 'images_me_o',
         'userTags': 'user_tags',
         'machineTags': 'machine_tags',
         'docId': 'document_id',
         'docType': 'protocol_type_i_o_me',
         "date": "date_i_o_me",
-        "images": "image_o_me"
+        "images": "images_me_o"
     }
     return _translate(es_segment, meta_alignement)
 
+
 def segment_light(es_segment):
     meta_alignement = {
-            "type": 'protocol_type_i_o_me',
-            "docId": 'document_id',
-            "fragmentId": 'text_segment_id',
-            'machineTags': 'machine_tags',
-            "question": 'text_question',
-            "answer": 'text_answer',
-            "date": "date_i_o_me"
-        }
+        "type": 'protocol_type_i_o_me',
+        "docId": 'document_id',
+        "fragmentId": 'text_segment_id',
+        'machineTags': 'machine_tags',
+        "question": 'text_question',
+        "answer": 'text_answer',
+        "date": "date_i_o_me"
+    }
     return _translate(es_segment, meta_alignement)
 
 
@@ -48,5 +50,6 @@ def document(es_doc, es_segments=[]):
     doc = _translate(es_doc, meta_alignement)
     if len(es_segments) > 0:
         doc['fragments'] = [segment(s) for s in es_segments]
-    doc["metadata"] = {f:v for f,v in es_doc.items() if f not in meta_alignement.values()}
+    doc["metadata"] = {f: v for f, v in es_doc.items(
+    ) if f not in meta_alignement.values()}
     return doc
