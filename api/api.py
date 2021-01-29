a361e3ebf623ce2e26336601875d5c9baf3344f3
Paul Girard <paul@ouestware.com>
2021-01-29 14:36:35 +0100

    switch to simple query syntax

--- api/api.py
+++ api/api.py
@@ -7,9 +7,11 @@ import csv
 import io
 app = Flask(__name__)
 
+
 @app.route('/ping')
 def ping():
     return 'pong'
+
 
 @app.route('/search')
 def search(method='GET'):
@@ -22,9 +24,10 @@ def search(method='GET'):
     if dates:
         date_min, date_max = dates.split('|')
     # demultiplex filters
-    filters = {field: value.split('|') for field, value in request.args.items() if field not in ["query", "size", "offset", "date"]}
+    filters = {field: value.split('|') for field, value in request.args.items(
+    ) if field not in ["query", "size", "offset", "date"]}
 
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
     search = {
         "from": offset,
         "size": size,
@@ -32,8 +35,8 @@ def search(method='GET'):
         "query": {
             "bool": {
                 "must": [{
-                    "query_string":  {
-                        "query": query ,
+                    "simple_query_string":  {
+                        "query": query,
                         "fields": ["search"]
                     }
                 }]
@@ -48,12 +51,13 @@ def search(method='GET'):
     # filters in ES
     # mutivalue => should
     # AND between filters
-    for f,values in filters.items():
-        filter_must = {"bool":{"should": [{"term": {f: v}} for v in values], "minimum_should_match":1, "boost":2}}
+    for f, values in filters.items():
+        filter_must = {"bool": {"should": [
+            {"term": {f: v}} for v in values], "minimum_should_match": 1, "boost": 2}}
         search["query"]["bool"]["must"].append(filter_must)
     # date in ES
     if dates:
-        search["query"]["bool"]["filter"]= [{"range": {"date_i_o_me": {}}}]
+        search["query"]["bool"]["filter"] = [{"range": {"date_i_o_me": {}}}]
         if date_max:
             search["query"]["bool"]["filter"][0]["range"]["date_i_o_me"]["lte"] = date_max
         if date_min:
@@ -63,11 +67,9 @@ def search(method='GET'):
 
     return {
         "total": results['hits']['total']['value'],
-        "results": [ dict(segment_light(s['_source']), **{"highlights": s['highlight']['search'] if 'highlight' in s and 'search' in s['highlight'] else []})
-            for s in results['hits']['hits']]
+        "results": [dict(segment_light(s['_source']), **{"highlights": s['highlight']['search'] if 'highlight' in s and 'search' in s['highlight'] else []})
+                    for s in results['hits']['hits']]
     }
-
-
 
 
 @app.route('/autocomplete')
@@ -79,14 +81,15 @@ def autocomplete(method='GET'):
     if not field:
         abort(404)
     terms = {
-                    "field":field,
-                    "size": size,
-                    "missing": "N/A"
-                }
+        "field": field,
+        "size": size,
+        "missing": "N/A"
+    }
     if query:
         # make the query case insensitive
-        terms["include"] = ".*%s.*"%("".join(["[%s%s]"%(c.lower(),c.upper()) for c in query]))
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+        terms["include"] = ".*%s.*" % ("".join(["[%s%s]" %
+                                                (c.lower(), c.upper()) for c in query]))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
     search = {
         "size": 0,
         "track_total_hits": True,
@@ -96,43 +99,46 @@ def autocomplete(method='GET'):
             }
         }
     }
-    results = es.search(search, index="text_segments")['aggregations']['suggestions']['buckets']
-    items = filter(lambda item: item != "", [r['key']  for r in results]);
-    return jsonify([ r for r in items])
+    results = es.search(search, index="text_segments")[
+        'aggregations']['suggestions']['buckets']
+    items = filter(lambda item: item != "", [r['key'] for r in results])
+    return jsonify([r for r in items])
 
-@app.route('/doc/<string:id>', methods= ['GET'])
+
+@app.route('/doc/<string:id>', methods=['GET'])
 def doc(id):
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
     try:
         doc = es.get('documents', id)['_source']
         # segments
         segment_search = {
             "size": 9999,
-            "sort": {"order":"asc"},
+            "sort": {"order": "asc"},
             "track_total_hits": True,
-            "query":{
-                "bool":{
+            "query": {
+                "bool": {
                     "filter": {
-                    "term": {"document_id": id}
-                }}
+                        "term": {"document_id": id}
+                    }}
             }
         }
-        segments = es.search(segment_search, index= 'text_segments')
+        segments = es.search(segment_search, index='text_segments')
         return document(doc, [s['_source'] for s in segments['hits']['hits']])
     except exceptions.NotFoundError:
         abort(404)
 
-@app.route('/similarSegments/<string:id>', methods= ['GET'])
+
+@app.route('/similarSegments/<string:id>', methods=['GET'])
 def similar_segments(id):
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
     try:
         segment = es.get('text_segments', id)['_source']
         similar_ids = segment['text_segment_similarity_id']
-        if similar_ids != "" and not isinstance(similar_ids,list):
+        if similar_ids != "" and not isinstance(similar_ids, list):
             similar_ids = [similar_ids]
-        if len(similar_ids)>0:
-            similar_segments = es.mget(index = 'text_segments',
-                    body = {'ids': similar_ids})
+        if len(similar_ids) > 0:
+            similar_segments = es.mget(index='text_segments',
+                                       body={'ids': similar_ids})
             return jsonify([segment_light(s['_source']) for s in similar_segments['docs']])
         else:
             return []
@@ -140,41 +146,43 @@ def similar_segments(id):
         abort(404)
 
 
-@app.route('/fragment/<string:id>', methods= ['POST'])
+@app.route('/fragment/<string:id>', methods=['POST'])
 def update_segment(id):
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
     try:
         # restrict update to user_tags field
         body = request.get_json()
         if "user_tags" not in body:
             abort(400)
-        doc = {"doc": {"user_tags" : body["user_tags"]}, "_source": True}
+        doc = {"doc": {"user_tags": body["user_tags"]}, "_source": True}
         seg = es.update('text_segments', id, doc)
         return segment(seg['get']['_source'])
 
     except exceptions.NotFoundError:
         abort(404)
 
+
 @app.route('/user_tags.csv')
 def user_tags_csv(method='GET'):
-    es = Elasticsearch('%s:%s'%(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
+    es = Elasticsearch('%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT))
 
     segs = es.search({
         "size": 9999,
-        "query":{
+        "query": {
             "bool": {
-            "must":
+                "must":
                 {
-                "exists": {
-                    "field": "user_tags"
+                    "exists": {
+                        "field": "user_tags"
+                    }
                 }
-                            }
             }
         },
-        "_source":["document_id", "text_segment_id", "user_tags"]
-    }, index= "text_segments")
-    csvString =  io.StringIO()
-    writer = csv.DictWriter(csvString,["document_id", "text_segment_id", "user_tags"])
+        "_source": ["document_id", "text_segment_id", "user_tags"]
+    }, index="text_segments")
+    csvString = io.StringIO()
+    writer = csv.DictWriter(
+        csvString, ["document_id", "text_segment_id", "user_tags"])
     writer.writeheader()
     writer.writerows(seg['_source'] for seg in segs['hits']['hits'])
     return Response(csvString.getvalue(), mimetype='text/csv')
